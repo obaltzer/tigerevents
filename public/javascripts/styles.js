cecc44d9032a30cc033afb795f3dd7f47be1dd55
Oliver Baltzer <oliver@baltzer.net>
2005-09-05 01:09:41 +0000

    Resolved #199 by adding browser detection from http://www.dithered.com/javascript/browser_detect/ and use stylesheet switching facility used be Plone to switch to an alternate stylesheet that does not use CSS2 selectors when the browser is IE. I also use the browser detection mechanism to disable JavaScript effects for IE and Konqueror. Safari needs to be tested.

--- public/javascripts/styles.js
+++ public/javascripts/styles.js
@@ -1,6 +1,14 @@
-Behaviour.register({
-    'ul#all_announcements_list': function(e)
-    {
-        new FadeItems(e);
-    },
-});
+if(browser.isGecko && !browser.isKonqueror || browser.isSafari 
+    || browser.isOpera)
+{
+    Behaviour.register({
+        'ul#all_announcements_list': function(e)
+        {
+            new FadeItems(e);
+        }
+    });
+}
+else
+{
+    setActiveStyleSheet('nofx', 1);
+}
