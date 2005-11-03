c487c41a0bd27d0a81560f5ed67ff312ad6252be
Sean Smith <ssmith@cs.dal.ca>
2005-11-03 21:31:30 +0000

    Removing obsolete sql data files from 0.6 branch and TRUNK

--- db/base_data.sql
+++
@@ -1,8 +0,0 @@
--- create the association types
-INSERT INTO association_types (name) VALUES ('OR');
-INSERT INTO association_types (name) VALUES ('AND');
-
-INSERT INTO selectors (name, label, include_events, include_announcements) VALUES ('Example Selector', 'example_selector', 1, 1);
-
-INSERT INTO layouts (user_id) VALUES (NULL);
-INSERT INTO layouts_selectors (layout_id, selector_id, rank) SELECT layouts.id, selectors.id, 100 FROM selectors, layouts WHERE layouts.user_id IS NULL;
